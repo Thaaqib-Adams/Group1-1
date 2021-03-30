3465928fd71d4a99561e2cdfffe289a95082633d
imraan-abdullah <219361738@mycput.ac.za>
2021-03-30 21:44:05 +0200

    program complete

--- src/main/java/za/ac/cput/shapeforms/main.java
+++ src/main/java/za/ac/cput/shapeforms/main.java
@@ -1,9 +1,28 @@
 package za.ac.cput.shapeforms;
+import java.util.Scanner;
 
 public class main {
 //We will call all of the classes from here, don't worry if there's nothing here
     public static void main(String[] args) {
-       
+        Scanner object = new Scanner(System.in);
+        
+        System.out.println("FORMULA FOR CALCULATING A RECTANGULAR PRISM");
+        System.out.println("******************************************* \n");
+        
+        System.out.println("1: Please input in the rectangular's length: ");
+        double lgt = object.nextDouble();
+        
+        System.out.println("2: Please input in the rectangular's width: ");
+        double wdt = object.nextDouble();
+        
+        System.out.println("3: Please input in the rectangular's height: ");
+        double hgt = object.nextDouble();
+        
+        Rectangular shape = new Rectangular(lgt, wdt, hgt);
+         
+        System.out.println(shape.displayResults());
+        shape.calculateVolume();
+        shape.calculateSurface();
     }
 
 }
--- src/test/java/za/ac/cput/shapeforms/demoTest.java
+++ src/test/java/za/ac/cput/shapeforms/demoTest.java
@@ -5,10 +5,16 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import junit.framework.Assert;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 //Please create your own Java file in the src/ folder and then create an associated Test file with it
 public class demoTest {
+
+    Rectangular shape = null;
+    Rectangular square = null;
 
     public demoTest() {
     }
@@ -23,10 +29,31 @@ public class demoTest {
 
     @BeforeEach
     public void setUp() {
+        double len = 5;
+        double wid = 5;
+        double height = 5;
+
+        square = new Rectangular(5, 5, 5);
+        shape = new Rectangular(len, wid, height);
+
+        shape = square;
+    }
+
+    @Test
+    public void calculateSurface()
+    {
+        double result = .0d;
+        result = shape.calculateSurface();
+        Assert.assertEquals(150, result, 0.1);
+    }
+
+    @Test
+    public void isObjectEqual()
+    {
+        Assert.assertEquals(shape, square);
     }
 
     @AfterEach
     public void tearDown() {
     }
 }
-
