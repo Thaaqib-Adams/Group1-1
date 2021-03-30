9e0aaba35b4ccb4c61ffa7d6dc8962d39ee97de2
Fayaad Abrahams <80263556+FayaadAbrahams@users.noreply.github.com>
2021-03-30 17:01:38 +0200

    Add files via upload

---
+++ src/main/java/za/ac/cput/shapeforms/main.java
@@ -0,0 +1,10 @@
+package za.ac.cput.shapeforms;
+
+public class main {
+//We will call all of the classes from here, don't worry if there's nothing here
+    public static void main(String[] args) {
+        Cylinder ob = new Cylinder();
+        System.out.println(ob.toString());
+    }
+
+}
