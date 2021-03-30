8f0113d6a038df4c5fa81af9447ee2fc3d6dddc4
Fayaad Abrahams <80263556+FayaadAbrahams@users.noreply.github.com>
2021-03-30 17:02:02 +0200

    Update main.java

--- src/main/java/za/ac/cput/shapeforms/main.java
+++ src/main/java/za/ac/cput/shapeforms/main.java
@@ -3,8 +3,7 @@ package za.ac.cput.shapeforms;
 public class main {
 //We will call all of the classes from here, don't worry if there's nothing here
     public static void main(String[] args) {
-        Cylinder ob = new Cylinder();
-        System.out.println(ob.toString());
+       
     }
 
 }
