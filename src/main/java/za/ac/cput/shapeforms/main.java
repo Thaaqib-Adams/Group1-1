06b29e4e844ee0fc5dbcfae282887d516c058a5f
Fayaad Abrahams <fayaad1999@gmail.com>
2021-03-30 16:02:31 +0200

    First Commit

---
+++ src/main/java/za/ac/cput/shapeforms/main.java
@@ -0,0 +1,9 @@
+package za.ac.cput.shapeforms;
+
+public class main {
+//We will call all of the classes from here, don't worry if there's nothing here
+    public static void main(String[] args) {
+
+    }
+
+}
