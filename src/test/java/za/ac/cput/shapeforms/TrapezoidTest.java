0135f4156186a0f16f0c3fa15ff874381c8b940e
Abel Kabeya <abelkofficial@gmail.com>
2021-03-30 19:33:44 +0200

    Trapezoid Test class populated

--- src/test/java/za/ac/cput/shapeforms/TrapezoidTest.java
+++ src/test/java/za/ac/cput/shapeforms/TrapezoidTest.java
@@ -11,30 +11,37 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Timeout;
 
 /**
  *
  * @author AbelK
  */
 public class TrapezoidTest {
-    
-    public TrapezoidTest() {
-    }
-    
-    @BeforeAll
-    public static void setUpClass() {
-    }
-    
-    @AfterAll
-    public static void tearDownClass() {
-    }
-    
+
+    private Trapezoid trapezoid1;
+    private Trapezoid trapezoid2;
+    private Trapezoid trapezoid3;
+
     @BeforeEach
     public void setUp() {
+
+        trapezoid1 = new Trapezoid();
+        trapezoid2 = new Trapezoid();
+        trapezoid3 = trapezoid1;
+
     }
-    
-    @AfterEach
-    public void tearDown() {
+
+    @Test
+    public void testIdentity() {
+        assertSame(trapezoid1, trapezoid3);
+    }
+
+    @Test
+    public void testEquality() {
+        assertEquals(trapezoid1, trapezoid3);
+
     }
 
     @Test
@@ -42,5 +49,19 @@ public class TrapezoidTest {
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
-    
+
+    @Timeout(100)
+    @Test
+    public void testTimeOut() {
+        System.out.println("Time meout");
+
+    }
+
+    @Disabled
+    @Test
+    public void testThisMethod() {
+        System.out.println("Test me");
+
+    }
+
 }
