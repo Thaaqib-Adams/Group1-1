62cd6ce5962e4ffd4a0c74a7e596a5d83b00c9eb
Fayaad Abrahams <80263556+FayaadAbrahams@users.noreply.github.com>
2021-03-30 16:56:02 +0200

    Delete src directory

--- src/main/java/za/ac/cput/shapeforms/main.java
+++
@@ -1,9 +0,0 @@
-package za.ac.cput.shapeforms;
-
-public class main {
-//We will call all of the classes from here, don't worry if there's nothing here
-    public static void main(String[] args) {
-
-    }
-
-}
