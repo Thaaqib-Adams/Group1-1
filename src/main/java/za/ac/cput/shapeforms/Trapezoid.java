af6b7fe14a772c2dad137ffbdbbfd1f380005e10
Abel Kabeya <abelkofficial@gmail.com>
2021-03-30 19:24:23 +0200

    Trapezoid Class Populated

--- src/main/java/za/ac/cput/shapeforms/Trapezoid.java
+++ src/main/java/za/ac/cput/shapeforms/Trapezoid.java
@@ -11,4 +11,31 @@ package za.ac.cput.shapeforms;
  */
 public class Trapezoid {
     
+private String area=" (base 1+base 2)/2*height";
+private String perimeter=" Add lenghts of all side";
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public String getPerimeter() {
+        return perimeter;
+    }
+
+    public void setPerimeter(String perimeter) {
+        this.perimeter = perimeter;
+    }
+
+    @Override
+    public String toString() {
+        return "Trapezoid\n" + "Area =" + area + "\nPerimeter=" + perimeter + "";
+    }
+
+
+
+    
 }
