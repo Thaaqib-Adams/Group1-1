62a86bdb2e6a5a5607c473b59c54fc572be3a7d8
Abel Kabeya <abelkofficial@gmail.com>
2021-03-30 19:21:04 +0200

    Trapezoid Class and Trapezoid Test class created

--- src/test/java/za/ac/cput/shapeforms/TrapezoidTest.java
+++ src/test/java/za/ac/cput/shapeforms/TrapezoidTest.java
@@ -1,3 +1,8 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package za.ac.cput.shapeforms;
 
 import org.junit.jupiter.api.AfterEach;
@@ -7,26 +12,35 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-//Please create your own Java file in the src/ folder and then create an associated Test file with it
-public class demoTest {
-
-    public demoTest() {
+/**
+ *
+ * @author AbelK
+ */
+public class TrapezoidTest {
+    
+    public TrapezoidTest() {
     }
-
+    
     @BeforeAll
     public static void setUpClass() {
     }
-
+    
     @AfterAll
     public static void tearDownClass() {
     }
-
+    
     @BeforeEach
     public void setUp() {
     }
-
+    
     @AfterEach
     public void tearDown() {
     }
-}
 
+    @Test
+    public void testSomeMethod() {
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
