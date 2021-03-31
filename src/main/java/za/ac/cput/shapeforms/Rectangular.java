d956daebbe9eb3de61563b5d0a1a556881d8be54
imraan-abdullah <219361738@mycput.ac.za>
2021-03-31 19:48:49 +0200

    Final update

--- src/main/java/za/ac/cput/shapeforms/Rectangular.java
+++ src/main/java/za/ac/cput/shapeforms/Rectangular.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package za.ac.cput.shapeforms;
-
+import java.util.Scanner;
 /**
  *
  * @author imraan_219361738
--- src/main/java/za/ac/cput/shapeforms/main.java
+++ src/main/java/za/ac/cput/shapeforms/main.java
@@ -1,5 +1,5 @@
 package za.ac.cput.shapeforms;
-import java.util.Scanner;
+
 
 /**
  *
@@ -8,8 +8,7 @@ import java.util.Scanner;
 
 public class main {
 //We will call all of the classes from here, don't worry if there's nothing here
-    public static void main(String[] args) {
-       
-    }
+public static void main(String[] args) {
 
+    }
 }
