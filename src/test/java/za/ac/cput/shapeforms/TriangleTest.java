e9dfaefd5dd9d4cc7bcfbe9d2bddd32cf2e3e07e
Tarren-Adams <214041794@mycput.ac.za>
2021-03-31 16:20:52 +0200

    Populated Tests class abd did the tests

--- src/test/java/za/ac/cput/shapeforms/TriangleTest.java
+++ src/test/java/za/ac/cput/shapeforms/TriangleTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package za.ac.cput.shapeforms;
 
 import org.junit.jupiter.api.AfterEach;
@@ -11,6 +6,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Timeout;
 
 /**
  *
@@ -18,9 +15,10 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 public class TriangleTest {
     
-    public TriangleTest() {
-    }
-    
+    private Triangle triangle1;
+    private Triangle triangle2;
+    private Triangle triangle3;
+
     @BeforeAll
     public static void setUpClass() {
     }
@@ -31,16 +29,43 @@ public class TriangleTest {
     
     @BeforeEach
     public void setUp() {
+        triangle1=new Triangle();
+        triangle2=new Triangle();
+        triangle3=triangle1;
     }
     
     @AfterEach
     public void tearDown() {
+    }
+    
+    @Test
+    public void testEquality(){
+        assertEquals(triangle1,triangle1);
+    }
+    
+    @Test
+    public void testIdentity(){
+        assertSame(triangle1,triangle3);
+    }
+    @Test
+    public void testNotSameIdetity(){
+        assertNotSame(triangle1,triangle2);
     }
 
     @Test
     public void testSomeMethod() {
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
+    }
+    @Test
+    @Timeout(50)
+    public void testTimeout(){
+        System.out.println("Timeed out");
+    }
+    @Disabled
+    @Test
+    public void testAnotherMethed(){
+        System.out.println("This method has been disabled");
     }
     
 }
