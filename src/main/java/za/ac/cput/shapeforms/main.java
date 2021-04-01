e08e39795fce9e5a63af313ff8914cb876e30047
Fayaad Abrahams <80263556+FayaadAbrahams@users.noreply.github.com>
2021-04-01 09:45:30 +0200

    Revert "Delete main.java"
    This reverts commit c73f873912d7e505660a3e876bd81b76b3f3720e.

---
+++ src/main/java/za/ac/cput/shapeforms/main.java
@@ -0,0 +1,14 @@
+package za.ac.cput.shapeforms;
+
+
+/**
+ *
+ * @author imraan_219361738
+ */
+
+public class main {
+//We will call all of the classes from here, don't worry if there's nothing here
+public static void main(String[] args) {
+
+    }
+}
